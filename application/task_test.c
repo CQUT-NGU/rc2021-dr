aa88518535914f6a9cd3576d9c81e1ccf8dac3ee
lihao <1500465194@qq.com>
2021-04-24 16:02:52 +0800

    Update test

--- application/task_test.c
+++ application/task_test.c
@@ -18,6 +18,7 @@
 
 #include "bsp.h"
 #include "bsp_delay.h"
+#include "cc.h"
 #include "cmsis_os.h"
 #include "ctrl.h"
 #include "main.h"
@@ -51,13 +52,17 @@ void servo_stop(void)
 void task_test(void *pvParameters)
 {
     const ctrl_rc_t *rc = ctrl_rc_point();
-    ctrl_pc_t *      pc = ctrl_pc_point();
+
+    ctrl_pc_t *pc = ctrl_pc_point();
+
+    cc_lpf_t lpf;
+    cc_lpf_init(&lpf, 0.1f, 0.002f);
 
     //servo_start();
 
-    __HAL_TIM_SET_PRESCALER(&htim8, 168);
-    __HAL_TIM_SetAutoreload(&htim8, 999);
-    __HAL_TIM_SetCompare(&htim8, TIM_CHANNEL_2, 500);
+    __HAL_TIM_SET_PRESCALER(&htim8, 167);
+    __HAL_TIM_SetAutoreload(&htim8, 76);
+    __HAL_TIM_SetCompare(&htim8, TIM_CHANNEL_2, 38);
 
     uint8_t flag_stop = 1;
 
@@ -71,6 +76,11 @@ void task_test(void *pvParameters)
             __HAL_TIM_SET_PRESCALER(&htim8, tmp);
             tmp = (int32_t)pc->y;
             __HAL_TIM_SetAutoreload(&htim8, tmp);
+            tmp = (int32_t)pc->z;
+            if (tmp == 0)
+            {
+                tmp = (int32_t)pc->y;
+            }
             __HAL_TIM_SetCompare(&htim8, TIM_CHANNEL_2, (tmp / 2));
             if (flag_stop)
             {
@@ -81,23 +91,24 @@ void task_test(void *pvParameters)
         }
 
         default:
-        {
-            if (rc->rc.s[1] == RC_SW_DOWN || rc->rc.s[0] == RC_SW_DOWN)
+            if (rc->rc.s[1] == RC_SW_DOWN && rc->rc.s[0] == RC_SW_DOWN)
             {
-                if (rc->rc.ch[3] > 10)
+                int16_t tmp = 75;
+                cc_lpf(&lpf, rc->rc.ch[2]);
+                if (rc->rc.ch[2] > 10)
                 {
                     GPIOC->BSRR = GPIO_PIN_6;
-                    __HAL_TIM_PRESCALER(&htim8, 660 - rc->rc.ch[3]);
+                    tmp -= (int16_t)lpf.out / 10;
                     if (flag_stop)
                     {
                         flag_stop = 0;
                         HAL_TIM_PWM_Start(&htim8, TIM_CHANNEL_2);
                     }
                 }
-                else if (rc->rc.ch[3] < -10)
+                else if (rc->rc.ch[2] < -10)
                 {
-                    GPIOC->BSRR = (uint32_t)GPIO_PIN_6 << 16;
-                    __HAL_TIM_PRESCALER(&htim8, 660 + rc->rc.ch[3]);
+                    GPIOC->BSRR = (uint32_t)GPIO_PIN_6 << 16U;
+                    tmp += (int16_t)lpf.out / 10;
                     if (flag_stop)
                     {
                         flag_stop = 0;
@@ -112,9 +123,18 @@ void task_test(void *pvParameters)
                         HAL_TIM_PWM_Stop(&htim8, TIM_CHANNEL_2);
                     }
                 }
+                __HAL_TIM_SetAutoreload(&htim8, tmp);
+                __HAL_TIM_SetCompare(&htim8, TIM_CHANNEL_2, tmp / 2);
+            }
+            else
+            {
+                if (!flag_stop)
+                {
+                    flag_stop = 1;
+                    HAL_TIM_PWM_Stop(&htim8, TIM_CHANNEL_2);
+                }
             }
             break;
-        }
         }
 
         osDelay(2U);
