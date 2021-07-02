caff47656a2077e5e3bd7998098f3876c84a1838
lihao <1500465194@qq.com>
2021-07-03 00:58:56 +0800

    feat(shoot): Add archery device control task

--- application/task_shoot.c
+++ application/task_shoot.c
@@ -3,9 +3,9 @@
  * @file         task_shoot.c
  * @brief        task shoot
  * @author       ngu
- * @date         20210509
+ * @date         20210703
  * @version      1
- * @copyright    Copyright (c) 2021
+ * @copyright    Copyright (c) 2021 NGU
  * *****************************************************************************
 */
 
@@ -19,52 +19,70 @@
 #endif /* USED_OS */
 
 /* chassis 3508 max motor control current */
-#define MAX_MOTOR_CAN_CURRENT 16000.0F
+#define MAX_MOTOR_CAN_CURRENT 0x4000
+
+/* M3508 rmp change to speed */
+/* x * 2 * PI / 60 / 19 */
+#define M3508_MOTOR_RPM_TO_VECTOR 0.005511566058929462F
+
+typedef enum
+{
+    SHOOT_STATS_RESET,  //!< reset
+    SHOOT_STATS_RUN,    //!< runnig
+    SHOOT_STATS_STOP,   //!< stop
+} shoot_stats_t;
 
 typedef struct
 {
+    shoot_stats_t  stats;
     const motor_t *motor;
-    ca_pid_f32_t   pid;
-    int16_t        out;
+    ca_pid_f32_t   pidv;
+    float          v;
+    float          v_set;
+    float          acc;
+    ca_pid_f32_t   pida;
     float          angle;
+    float          angle_set;
     float          angle_min;
     float          angle_max;
+    int16_t        out;
 } shoot_t;
 
 shoot_t shoot;
 
 void shoot_angle_update(motor_t *mo)
 {
-    int16_t delta = (int16_t)(mo->ecd_last - mo->ecd);
+    int delta = (int)(mo->ecd_last - mo->ecd);
     if (delta > ECD_RANGE_HALF)
     {
-        delta -= ECD_RANGE;
+        delta = (int)(delta - ECD_RANGE);
     }
     else if (delta < -ECD_RANGE_HALF)
     {
-        delta += ECD_RANGE;
+        delta = (int)(delta + ECD_RANGE);
     }
-    shoot.angle += MOTOR_ROLLER_RAD * delta;
-    if (shoot.angle > (float)M_PI * 2)
-    {
-        shoot.angle -= (float)M_PI * 2;
-    }
+    shoot.angle += MOTOR_ROLLER_RAD * (float)delta;
 }
 
 static void shoot_cli(shoot_t *pdata)
 {
     static float angle = 0;
-    while (1)
+
+    uint16_t count = 100;
+
+    do
     {
-        other_ctrl(1000, 0, 0, 0);
-        osDelay(100);
+        other_ctrl(800, 0, 0, 0);
+
+        osDelay(50);
+
         if (*(int32_t *)&angle == *(int32_t *)&pdata->angle)
         {
             pdata->angle = 0;
             break;
         }
         angle = pdata->angle;
-    }
+    } while (--count);
 }
 
 void task_shoot(void *pvParameters)
@@ -73,30 +91,120 @@ void task_shoot(void *pvParameters)
 
     osDelay(1000);
 
-    const float kpid[3] = {
-        3000,
-        0,
-        10000,
-    };
+    /* Initialization block */
+    {
+        /* Get motor data address */
+        shoot.motor = chassis_point(4);
+        /* Speed pid */
+        const float kpidv[3] = {
+            3000,
+            10,
+            0,
+        };
+        /* Angle pid */
+        const float kpida[3] = {
+            1000,
+            1,
+            0,
+        };
+        /* Speed pid initialization */
+        ca_pid_f32_position(&shoot.pidv,
+                            kpidv,
+                            -MAX_MOTOR_CAN_CURRENT,
+                            MAX_MOTOR_CAN_CURRENT,
+                            2000);
+        /* Angle pid initialization */
+        ca_pid_f32_position(&shoot.pida,
+                            kpida,
+                            -MAX_MOTOR_CAN_CURRENT,
+                            MAX_MOTOR_CAN_CURRENT,
+                            2000);
+        /* Set the limit of the angle */
+        shoot.angle_min = 0;
+        shoot.angle_max = (float)M_PI;
+        /* Start to calibrate the zero point */
+        //shoot_cli(&shoot);
+    }
 
-    shoot.motor     = chassis_point(4);
-    shoot.angle_min = 0;
-    shoot.angle_max = (float)M_PI;
-    ca_pid_f32_position(&shoot.pid,
-                        kpid,
-                        -MAX_MOTOR_CAN_CURRENT,
-                        MAX_MOTOR_CAN_CURRENT,
-                        2000);
-
-    //shoot_cli(&shoot);
+    /* The data address of the host computer */
+    ctrl_pc_t *pc = (ctrl_pc_t *)ctrl_pc_point();
 
     while (1)
     {
-        // shoot.out = (int16_t)ca_pid_f32(&shoot.pid, 2, shoot.angle);
-        // other_ctrl(shoot.out, 0, 0, 0);
+        /* Update speed and acceleration data */
+        {
+            float v_tmp = shoot.motor->v_rpm * M3508_MOTOR_RPM_TO_VECTOR;
+            shoot.acc   = shoot.v - v_tmp;
+            shoot.v     = v_tmp;
+        }
 
-        // os_printf("%g %i\r\n", shoot.angle, shoot.out);
+        /* Process the data of the host computer */
+        {
+            if (pc->c == 'a')
+            {
+                pc->c       = 0;
+                shoot.v_set = pc->x;
+                shoot.stats = SHOOT_STATS_RUN;
+            }
+        }
+
+        /* Output calculation block */
+        {
+            if (shoot.stats == SHOOT_STATS_RUN)
+            {
+                if (shoot.angle < 2)
+                {
+                    shoot.out = (int16_t)ca_pid_f32(&shoot.pidv,
+                                                    shoot.v_set,
+                                                    shoot.v);
+                }
+                else
+                {
+                    shoot.stats = SHOOT_STATS_STOP;
+                }
+            }
+
+            if (shoot.stats == SHOOT_STATS_STOP)
+            {
+                if (shoot.v_set > 0.01F)
+                {
+                    shoot.v_set -= 0.01F;
+                }
+                else
+                {
+                    shoot.v_set = 0;
+                }
+
+                shoot.out = (int16_t)ca_pid_f32(&shoot.pidv,
+                                                shoot.v,
+                                                shoot.v_set);
+
+                if (-0.01F < shoot.acc && shoot.acc < 0.01F)
+                {
+                    shoot.stats = SHOOT_STATS_RESET;
+                    //shoot_cli(&shoot);
+                }
+            }
+
+            if (shoot.stats == SHOOT_STATS_RESET)
+            {
+                shoot.out = (int16_t)ca_pid_f32(&shoot.pida,
+                                                0,
+                                                shoot.angle);
+            }
+        }
+
+        /* Control motor */
+        other_ctrl(shoot.out, 0, 0, 0);
+
+        // os_printf("%i,%g,%g,%g\r\n",
+        //           shoot.out,
+        //           shoot.v,
+        //           shoot.acc,
+        //           shoot.angle);
 
         osDelay(2);
     }
 }
+
+/************************ (C) COPYRIGHT NGU ********************END OF FILE****/
