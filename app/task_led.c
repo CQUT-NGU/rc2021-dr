c8a85c110dfba873e088055b8b0a39d62927fbc7
lihao <1500465194@qq.com>
2021-07-13 13:44:43 +0800

    build: 改变目录名称

--- app/task_led.c
+++ app/task_led.c
@@ -26,65 +26,43 @@ void task_led(void *pvParameters)
     (void)pvParameters;
 
     buzzer_start();
-    buzzer_set(0, BUZZER_PWM_DIV64);
-    osDelay(1000);
-    buzzer_set(0, 0);
+    {
+        uint32_t i = 0;
+        while (i != 8)
+        {
+            buzzer_set(i++, BUZZER_PWM_DIV2);
+            osDelay(0x80);
+        }
+        while (i)
+        {
+            buzzer_set(--i, BUZZER_PWM_DIV2);
+            osDelay(0x80);
+        }
+    }
     buzzer_stop();
 
     led_pwm_start();
 
-    uint8_t t = 0;
-    uint16_t count;
-
     for (;;)
     {
-        for (count = 0; count != LED_PWM_MAX; ++count)
+        for (uint16_t count = 0; count != LED_PWM_MAX; ++count)
         {
             led_pwm_set(LED_R, count);
             led_pwm_set(LED_B, (uint16_t)(LED_PWM_MAX - count));
             osDelay(1);
         }
-        for (count = 0; count != LED_PWM_MAX; ++count)
+        for (uint16_t count = 0; count != LED_PWM_MAX; ++count)
         {
             led_pwm_set(LED_G, count);
             led_pwm_set(LED_R, (uint16_t)(LED_PWM_MAX - count));
             osDelay(1);
         }
-        for (count = 0; count != LED_PWM_MAX; ++count)
+        for (uint16_t count = 0; count != LED_PWM_MAX; ++count)
         {
             led_pwm_set(LED_B, count);
             led_pwm_set(LED_G, (uint16_t)(LED_PWM_MAX - count));
             osDelay(1);
         }
-
-#if 0
-        if (t == 1)
-        {
-            move.data_pc->c = 'p';
-            move.data_pc->x = 0;
-            move.data_pc->y = 1;
-        }
-        else if (t == 2)
-        {
-            move.data_pc->c = 'p';
-            move.data_pc->x = 1;
-            move.data_pc->y = 1;
-        }
-        else if (t == 3)
-        {
-            move.data_pc->c = 'p';
-            move.data_pc->x = 1;
-            move.data_pc->y = 0;
-        }
-        else if (t == 4)
-        {
-            move.data_pc->c = 'p';
-            move.data_pc->x = 0;
-            move.data_pc->y = 0;
-            t               = 0;
-        }
-#endif
-        ++t;
     }
 }
 
